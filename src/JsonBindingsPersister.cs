30e406a7b4fc42ace7310a7248070e6cb00d72b0
Mads Kristensen <post@madskristensen.net>
2015-08-03 13:01:31 -0700

    Fixed format for VS event bindings

--- src/JsonBindingsPersister.cs
+++ src/JsonBindingsPersister.cs
@@ -1,45 +1,65 @@
-﻿using System.Linq;
 using AlfredTrx.Helpers;
-using Newtonsoft.Json.Linq;
 
-namespace NpmTaskRunner
+namespace BroccoliTaskRunner
 {
     public class JsonBindingsPersister : IPersistTaskRunnerBindings
     {
         public string FormatBindingsXmlForPersistence(string bindingsXml)
         {
-            return "  \"-vs-bindings\": " + "\"" + bindingsXml + "\",";
+            return "///" + bindingsXml;
         }
 
         public bool TryGetBindingsFromLine(string line, out LineRange rangeInLine)
         {
-            rangeInLine = new LineRange { Start = 0, Length = line.Length };
+            int hashIndex = line.IndexOf("///");
 
-            if (string.IsNullOrEmpty(line) || line.Length < 20 || !line.Contains("-vs-bindings"))
-                return false;
-
-            try
+            if (hashIndex < 0)
             {
-                JObject root = JObject.Parse("{" + line.TrimEnd(',') + "}");
-
-                var element = root.Children<JProperty>().First();
-
-                if (element.Name != "-vs-bindings")
-                    return false;
-
-                return true;
-            }
-            catch
-            {
+                rangeInLine = new LineRange();
                 return false;
             }
+
+            int openIndex = line.IndexOf("<binding", hashIndex);
+
+            if (openIndex < 0)
+            {
+                rangeInLine = new LineRange();
+                return false;
+            }
+
+            hashIndex = line.LastIndexOf("///", openIndex);
+
+            if (hashIndex < 0)
+            {
+                rangeInLine = new LineRange();
+                return false;
+            }
+
+            int closeIndex = line.IndexOf("/>", openIndex);
+
+            if (closeIndex < 0)
+            {
+                rangeInLine = new LineRange();
+                return false;
+            }
+
+            //The tag can be closed tightly or have a space after the element name <binding/> or <binding ... />
+            if (closeIndex != openIndex + 8 && !char.IsWhiteSpace(line[openIndex + 8]))
+            {
+                rangeInLine = new LineRange();
+                return false;
+            }
+
+            rangeInLine = new LineRange { Start = hashIndex, Length = closeIndex + 2 - hashIndex };
+            return true;
         }
 
         public string UnformatBindingsXmlForPersistence(string line, LineRange rangeInLine)
         {
             string interestingPart = line.Substring(rangeInLine.Start, rangeInLine.Length);
             int bindingStart = interestingPart.IndexOf("<binding");
-            return interestingPart.Substring(bindingStart, rangeInLine.Length - bindingStart).TrimEnd('"', ',');
+            return interestingPart.Substring(bindingStart, rangeInLine.Length - bindingStart);
         }
+
     }
 }
