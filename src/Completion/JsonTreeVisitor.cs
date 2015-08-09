bcd836d932760b4c59cfa58448c64adb8e8b1f4d
Mads Kristensen <post@madskristensen.net>
2015-08-09 11:27:04 -0700

    Added JSON Intellisense

---
+++ src/Completion/JsonTreeVisitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.CSS.Core;
+using Microsoft.JSON.Core.Parser;
+using Microsoft.CSS.Core.Parser;
+
+namespace NpmTaskRunner
+{
+    internal class JSONItemCollector<T> : IJSONSimpleTreeVisitor where T : JSONParseItem
+    {
+        public IList<T> Items { get; private set; }
+        private bool _includeChildren;
+
+        public JSONItemCollector() : this(false) { }
+
+        public JSONItemCollector(bool includeChildren)
+        {
+            _includeChildren = includeChildren;
+            Items = new List<T>();
+        }
+
+        public VisitItemResult Visit(JSONParseItem parseItem)
+        {
+            var item = parseItem as T;
+
+            if (item != null)
+            {
+                Items.Add(item);
+                return (_includeChildren) ? VisitItemResult.Continue : VisitItemResult.SkipChildren;
+            }
+
+            return VisitItemResult.Continue;
+        }
+    }
+}
---
+++ src/Completion/TaskCompletionProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using Microsoft.JSON.Core.Parser;
+using Microsoft.JSON.Core.Parser.TreeItems;
+using Microsoft.JSON.Editor.Completion;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+namespace NpmTaskRunner
+{
+    [Export(typeof(IJSONCompletionListProvider))]
+    [Name("NpmTaskCompletionProvider")]
+    internal class TaskCompletionProvider : IJSONCompletionListProvider
+    {
+        private const string FILENAME = "package.json";
+        private const string ELEMENT_NAME = "-vs-bindings";
+
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        public JSONCompletionContextType ContextType
+        {
+            get { return JSONCompletionContextType.ArrayElement; }
+        }
+
+        public IEnumerable<JSONCompletionEntry> GetListEntries(JSONCompletionContext context)
+        {
+            ITextDocument document;
+            if (TextDocumentFactoryService.TryGetTextDocument(context.Snapshot.TextBuffer, out document))
+            {
+                string fileName = Path.GetFileName(document.FilePath).ToLowerInvariant();
+
+                if (string.IsNullOrEmpty(fileName) || !fileName.Equals(FILENAME, StringComparison.OrdinalIgnoreCase))
+                    yield break;
+            }
+            else
+            {
+                yield break;
+            }
+
+            JSONMember member = context.ContextItem.FindType<JSONMember>();
+
+            while (member != null && member.Parent != null && member.UnquotedNameText != ELEMENT_NAME)
+            {
+                member = member.Parent.FindType<JSONMember>();
+            }
+
+            if (member == null || member.UnquotedNameText != ELEMENT_NAME)
+                yield break;
+
+            foreach (var task in GetTasks(context.ContextItem))
+            {
+                yield return new SimpleCompletionEntry(task.Item1, task.Item2, context.Session);
+            }
+        }
+
+        public static IEnumerable<Tuple<string, string>> GetTasks(JSONParseItem item)
+        {
+            var visitor = new JSONItemCollector<JSONMember>(true);
+            item.JSONDocument.Accept(visitor);
+
+            var scripts = visitor.Items.FirstOrDefault(member => member.UnquotedNameText == "scripts");
+
+            if (scripts == null)
+                yield break;
+
+            foreach (JSONObject child in scripts.Children.Where(c => c is JSONObject))
+            {
+                foreach (JSONMember taskItem in child.Children.Where(c => c is JSONMember))
+                {
+                    yield return Tuple.Create(taskItem.UnquotedNameText, taskItem.Value.ToString());
+                }
+            }
+        }
+    }
+}
