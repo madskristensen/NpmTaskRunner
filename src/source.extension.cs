e27590dd1ebd68b7e3f1ae9f4a65a9f3bfff3244
Mads Kristensen <post@madskristensen.net>
2016-03-18 10:52:19 -0700

    v1.2

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -5,7 +5,7 @@ namespace NpmTaskRunner
 		public const string Id = "d7f89ba3-815c-4feb-89b9-68d1654e2138";
 		public const string Name = "NPM Scripts Task Runner";
 		public const string Description = "Adds support for package.json's NPM scripts in the Task Runner Explorer";
-		public const string Version = "1.1";
+		public const string Version = "1.2";
 		public const string Author = "Mads Kristensen";
 		public const string Tags = "npm, package.json, task runner, scripts";
 	}
