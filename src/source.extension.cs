57076bcd51bb5f09834f60bdd39008924d492288
Mads Kristensen <post@madskristensen.net>
2016-04-21 09:27:43 -0700

    Updated description

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace NpmTaskRunner
     {
         public const string Id = "d7f89ba3-815c-4feb-89b9-68d1654e2138";
         public const string Name = "NPM Task Runner";
-        public const string Description = "Adds support for package.json's NPM scripts in the Task Runner Explorer";
+        public const string Description = "Adds support for npm scripts defined in package.json directly in Visual Studio's Task Runner Explorer";
         public const string Language = "en-US";
         public const string Version = "1.3";
         public const string Author = "Mads Kristensen";
