f10dd744daf40d8f4b87c6b6ad67a4a7c29d85db
Mads Kristensen <post@madskristensen.net>
2016-08-30 10:06:44 -0700

    Improved package load

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -2,15 +2,16 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace NpmTaskRunner
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [Guid(PackageGuids.guidVSPackageString)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad("{349C5852-65DF-11dA-9384-00065B846F21}", PackageAutoLoadFlags.BackgroundLoad)] // WAP
+    [ProvideAutoLoad("{E24C65DC-7377-472b-9ABA-BC803B73C61A}", PackageAutoLoadFlags.BackgroundLoad)] // WebSite
+    [ProvideAutoLoad("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}", PackageAutoLoadFlags.BackgroundLoad)] // ProjectK
+    [ProvideAutoLoad("{895DF540-DA8B-49B6-BD64-32FFD1410798}", PackageAutoLoadFlags.BackgroundLoad)] // Cordova
     [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class VSPackage : AsyncPackage
     {
