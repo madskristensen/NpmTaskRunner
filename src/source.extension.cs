0f1cbf8711a30c28ecda9d9a4d926401010e6997
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-10-18 16:52:19 -0700

    Added support for VS 2022

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -7,11 +7,11 @@ namespace NpmTaskRunner
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "d7f89ba3-815c-4feb-89b9-68d1654e2138";
+        public const string Id = "c5203f5f-4785-4c09-b383-688a100a9f5d";
         public const string Name = "NPM Task Runner";
         public const string Description = @"Adds support for npm scripts defined in package.json directly in Visual Studio's Task Runner Explorer. Includes full support for Yarn";
         public const string Language = "en-US";
-        public const string Version = "1.4";
+        public const string Version = "1.5";
         public const string Author = "Mads Kristensen";
         public const string Tags = "npm, package.json, task runner, scripts, yarn";
     }
